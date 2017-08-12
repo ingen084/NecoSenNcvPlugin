e8b654183a46705a412cf3a5c13bd962fc685b42
ingen084 <ingen188@gmail.com>
2017-08-12 15:10:18 +0900

    DLとかもろもろ書いた

--- NecoSenNcvPlugin/NcvPlugin.cs
+++ NecoSenNcvPlugin/NcvPlugin.cs
@@ -2,6 +2,12 @@
 using Plugin;
 using System.Windows.Forms;
 using System.Linq;
+using System.Xml.Serialization;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Xml;
 
 namespace NecoSenNcvPlugin
 {
@@ -16,13 +22,39 @@ namespace NecoSenNcvPlugin
 
 		public void AutoRun()
 		{
-			var pluginsMenu = (ToolStripMenuItem)Host.MainForm.MainMenuStrip.Items.Find("プラグイン(&P)", false).First();
-			var pluginMenu = (ToolStripMenuItem)pluginsMenu.DropDownItems.Find(Name, false).First();
-			pluginMenu.DropDownItems.Add("東方ch");
+			var pluginsMenu = Host.MainForm.MainMenuStrip.Items.OfType<ToolStripMenuItem>().First(mi => mi.Text == "プラグイン(&P)");
+			var pluginMenu = pluginsMenu.DropDownItems.OfType<ToolStripMenuItem>().First(mi => mi.Text == Name);
+			using (var reader = new XmlTextReader("https://ingen084.github.io/versioncheck/NECOSenNcvPlugin.xml"))
+			{
+				var serializer = new XmlSerializer(typeof(NecoSenPluginLiveInfos));
+				var info = (NecoSenPluginLiveInfos)serializer.Deserialize(reader);
+				if (info.Version > int.Parse(Version))
+					if (MessageBox.Show("プラグインの更新があります。更新ページを開きますか？", "NECOSen NCVプラグイン", MessageBoxButtons.YesNo) == DialogResult.Yes)
+						Process.Start("http" + info.UpdateUrl);
+				foreach (var liveInfo in info.Infos)
+					pluginMenu.DropDownItems.Add(new ToolStripMenuItem(liveInfo.Name, null, (s, e) => Host.ConnectLive(liveInfo.ChannelId)));
+			}
+
 		}
 
 		public void Run()
 		{
 		}
+	}
+
+	public class NecoSenPluginLiveInfos
+	{
+		public int Version { get; set; }
+		public string UpdateUrl { get; set; }
+		[XmlArray("Lives")]
+		[XmlArrayItem("Live")]
+		public NecoSenPluginLiveInfo[] Infos { get; set; }
+	}
+	public class NecoSenPluginLiveInfo
+	{
+		[XmlAttribute]
+		public string Name { get; set; }
+		[XmlAttribute]
+		public string ChannelId { get; set; }
 	}
 }
