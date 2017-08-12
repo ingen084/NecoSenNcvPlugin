bb3df3b9b7a633263901785fdb550f8b0d1d8f7a
ingen084 <ingen188@gmail.com>
2017-08-12 14:11:57 +0900

    プロジェクト ファイルを追加します。

---
+++ NecoSenNcvPlugin/NcvPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Plugin;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace NecoSenNcvPlugin
+{
+	public class NcvPlugin : IPlugin
+	{
+		public IPluginHost Host { get; set; }
+		public bool IsAutoRun => true;
+
+		public string Name => "&NECOSen";
+		public string Description => "NCVからNECOSenに接続しやすくするプラグインです。";
+		public string Version => "0";
+
+		public void AutoRun()
+		{
+			var pluginsMenu = (ToolStripMenuItem)Host.MainForm.MainMenuStrip.Items.Find("プラグイン(&P)", false).First();
+			var pluginMenu = (ToolStripMenuItem)pluginsMenu.DropDownItems.Find(Name, false).First();
+			pluginMenu.DropDownItems.Add("東方ch");
+		}
+
+		public void Run()
+		{
+		}
+	}
+}
